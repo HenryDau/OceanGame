81d6047655e5232e2cd729a916116eb64b6c615d
Charles Widdicombe <cwiddico@mymail.mines.edu>
2017-05-25 18:11:43 -0600

    Point counter implemented.

--- Assets/scripts/PointCounter.cs
+++ Assets/scripts/PointCounter.cs
@@ -1,15 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PointCounter : MonoBehaviour {
 
     //This holds the points. There might be a better way to do this.
     public int point = 0;
-
-    void Start()
-    {
+    public Text pointText;
+    void Start(){
+        point = 0;
+        pointText.text = "Points: " + point.ToString();
     }
 
-   
+    private void Update()
+    {
+        pointText.text = "Points: " + point.ToString();
+    }
 }
