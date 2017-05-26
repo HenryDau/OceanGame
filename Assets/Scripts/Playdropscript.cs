f3a3c03c784b60bd3175aecb25d88655855d4d15
Charles Widdicombe <cwiddico@mymail.mines.edu>
2017-05-25 21:30:27 -0600

    Implemented Sound on click for cup. More a test of concept, but notably it has to use an external file if its being destroyed.

---
+++ Assets/Scripts/Playdropscript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Playdropscript : MonoBehaviour {
+    public AudioClip WaterDrop;
+    public AudioSource source;
+
+    // Use this for initialization
+    void Start () {
+        source = GetComponent<AudioSource>();
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+
+    public void play()
+    {
+        source.PlayOneShot(WaterDrop, 1f);
+    }
+}
