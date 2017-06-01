8a44a99b4b4b37efb1ed8ec3f0fb81b6f8507243
Charles Widdicombe <cwiddico@mymail.mines.edu>
2017-06-01 10:27:11 -0600

    special fish make a sparkly noise

---
+++ Assets/Playsparklescript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Playsparklescript : MonoBehaviour
+{
+    public AudioClip sparkle;
+    private AudioSource source;
+
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+
+    }
+
+    public void play()
+    {
+        source.PlayOneShot(sparkle, 1f);
+    }
+}
